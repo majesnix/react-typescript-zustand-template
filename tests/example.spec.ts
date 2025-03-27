77db9cc2d81bd0ab8acddc3dc846f8539a5b3733
majesnix <majesnix@electronicintelligence.de>
2025-03-27 16:42:01 +0100

    chore(tests): cypress to playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+
+import { Server } from "miragejs";
+import { makeServer } from "../src/mock/mock.server"
+
+test.describe("init spec", () => {
+  let server: Server;
+
+  test.beforeEach(async ({ page }) => {
+    server = makeServer("test");
+  });
+
+  test("should load init text", async ({ page }) => {
+    server.create("example", { id: "1", header: "init" });
+    await page.goto("http://localhost:5173/");
+    await expect(page.getByText("init")).toBeVisible();
+  });
+});
